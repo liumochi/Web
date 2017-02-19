d327252b8f7a9cde4374c78b61ff405c684bfc4d
liumochi <m18846089770@163.com>
2017-02-19 10:28:28 +0800

    2.19 require.js

---
+++ require/b.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/2/19.
+ */
+define(function () {
+   function isArray(arr){
+       return arr instanceof Array;
+   }
+    return isArray;
+});
---
+++ require/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2017/2/19.
+ */
+require(["a"],function(arraySort){
+    var arr=[1,5,8,4,2,3,10];
+    console.log(arraySort(arr));
+});
